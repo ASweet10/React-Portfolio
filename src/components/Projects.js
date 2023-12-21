98f403f1243e498f855da2ca7a42f4a549f489b3
ASweet10 <asweet1234@yahoo.com>
2023-12-21 13:07:35 -0600

    Updated images

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,36 +1,35 @@
 import React from 'react'
-import stockDashboard from '../images/stock-dashboard-2.png'
-import ecommerce from '../images/ecommerce.png'
-import sortVisualizer from '../images/sort-visualizer-1.png'
-import gymWebsite from '../images/bg_image_forest.jpg'
+import stockDashboardOne from '../images/stock-dashboard-1.png'
+import stockDashboardTwo from '../images/stock-dashboard-2.png'
+import ecommerce1 from '../images/ecommerce1.png'
+import ecommerce2 from '../images/ecommerce2.png'
+import gymSiteBlog from '../images/MERN-gym-blog.png'
+import gymSiteClasses from '../images/MERN-gym-classes.png'
 import { FaGithub } from 'react-icons/fa'
-import { BsBox, BsBoxArrowUpRight } from 'react-icons/bs'
+import { BsBoxArrowUpRight } from 'react-icons/bs'
 
 export default function Projects() {
-  const projectTags = [
-    
-  ]
+
   const projects = [
-    { id: 0, src: ecommerce, title: 'E-Commerce', tools:[ "HTML", "CSS", "Next.JS", "Sanity", "Stripe" ],
-      desc: 'E-Commerce site built using Next.JS and Sanity CMS. User can add/remove/update items' +
-      ' in cart and use Stripe to complete payments.',
+    { id: 0, src1: ecommerce1, src2: ecommerce2, title: 'E-Commerce', tools:[ "HTML/CSS", "React", "Tailwind", "Sanity" ],
+      desc: 'E-Commerce site built using React and Sanity CMS. User can add/remove/update items in cart',
       demoUrl: '',
       sourceURL: 'https://github.com/ASweet10/ecommerce',
     },
-    { id: 1, src: gymWebsite, title: 'Gym Website', tools:[],
-      desc: '',
-      demoUrl: '',
-      sourceURL: '',
+    { id: 1, src1: gymSiteBlog, src2: gymSiteClasses, title: 'Gym Website', tools:[ "HTML/CSS", "React", "Tailwind", "MongoDB", "Express" ],
+      desc: 'Gym website built using the MERN stack. Learned how to use AWS S3 and CloudFront CDN to serve blog images.',
+      demoUrl: 'https://mern-gym-frontend-asweet10.vercel.app',
+      sourceURL: 'https://github.com/ASweet10/MERN-gym',
     },
-    { id: 2, src: gymWebsite, title: 'Social Media Site', tools:[ "HTML", "CSS", "React", "Tailwind", "MongoDB" ],
+    { id: 2, src1: gymSiteBlog, src2: gymSiteClasses, title: 'Social Media Site', tools:[ "HTML/CSS", "React", "Tailwind", "MongoDB", "Express" ],
       desc: '',
       demoUrl: 'https://sorting-visualizer-107.netlify.app',
       sourceURL: 'https://github.com/ASweet10/SortingVisualizer',
     },    
-    { id: 3, src: stockDashboard, title: 'Stock Dashboard', tools:[ "Python", "DASH", "Plotly.js" ],
+    { id: 3, src1: stockDashboardOne, src2: stockDashboardTwo, title: 'Stock Dashboard', tools:[ "Python", "DASH", "Plotly.js" ],
       desc: 'Stock market investment tool which includes price chart, metrics, investor sentiment,' + 
       ' volume visualization, and news articles.',
-      demoUrl: '',
+      demoUrl: 'https://stockdashboard-406v.onrender.com',
       sourceURL: 'https://github.com/ASweet10/StockDashboard',
     },
   ]
@@ -44,11 +43,20 @@ export default function Projects() {
           </div>
 
           <div className='flex flex-col gap-12'>
-            {projects.map(({id, src, title, desc, demoUrl, sourceURL, tools}) => {
+            {projects.map(({id, src1, src2, title, desc, demoUrl, sourceURL, tools}) => {
               return(
                 <div key={id} className='flex flex-col md:flex-row'>
-                  <img src={src} alt="" className='rounded-md h-48 w-96 object-cover' />
-                  <div className='flex flex-col'>
+                  <div className='rounded-md h-40 md:h-72 md:w-1/2 w-full object-cover overflow-hidden relative'>
+                    <img src={ src1 }  alt="Something went wrong" 
+                        className='w-full h-full object-cover absolute z-10 transition-transform duration-300 transform hover:scale-110'
+                    />
+                    <img src={ src2 } alt="Something went wrong"
+                        className='w-full h-full object-cover absolute z-20 opacity-0 transition-opacity duration-300 transform hover:opacity-100'
+                    />
+                  </div>
+
+                  {/* <img src={src1} alt="" className='rounded-md h-48 w-96 object-cover' /> */}
+                  <div className='flex flex-col md:w-1/2 w-full'>
                     <h1 className='text-left pl-6 pt-3 md:pt-0 text-2xl font-bold'>{title}</h1>
                     <div>
                       <div className='mx-6 my-6 text-lg'><p>{desc}</p></div>
